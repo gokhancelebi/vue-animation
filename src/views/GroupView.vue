e94ba42dd01396f8ff4ef895a29f9955304a42df
Gokhan <dinatopya03@gmail.com>
2023-08-27 19:01:45 +0300

    first commit

---
+++ src/views/GroupView.vue
@@ -0,0 +1,69 @@
+<script setup>
+import {reactive,ref} from 'vue'
+const items = reactive({items: [
+    {name : "Gökhan Çelebi",id:1},
+    {name : "Emirhan Çelebi", id:2},
+    {name: "Turhan Çelebi", id:3}
+]})
+
+let last_id = 3
+
+const itemName = ref("")
+const addNewItem = (e) => {
+    last_id++
+    items.items.unshift({
+        name: itemName.value,
+        id: last_id
+    })
+    itemName.value=""
+}
+
+const deleteItem = (itemId) => {
+    items.items = items.items.filter(item => item.id != itemId)
+}
+</script>
+<template>
+    <div class="container mx-auto flex flex-col justify-center items-center">
+        <div class="flex flex-col justify-center items-center gap-10 w-full mt-10">
+            <input @keypress.enter="addNewItem" v-model="itemName" type="text" class="shadow-md w-1/4 p-2">
+            <ul class="flex flex-col gap-3 w-1/4 justify-center items-center relative">
+                <TransitionGroup name="items">
+                    <li v-for="item in items.items" :key="item.id" @click="deleteItem(item.id)" 
+                    class="border shadow-md text-center rounded p-2 cursor-pointer w-full hover:bg-gray-50"
+                    >{{ item.name }}</li>
+                </TransitionGroup>
+            </ul>
+        </div>
+    </div>
+</template>
+<style scoped>
+.items-enter-from{
+    opacity: 0;
+    transform: scale(0.5);
+}
+.items-enter-to{
+    opacity: 1;
+    transform: scale(1);
+}
+.items-enter-active{
+    transition: all 0.5s ease;
+}
+
+.items-leave-from{
+    opacity: 1;
+    transform: scale(1);
+}
+.items-leave-to{
+    opacity: 0;
+    transform: scale(0.5);
+}
+.items-leave-active{
+    transition: all 0.5s ease;
+    position: absolute;
+}
+
+.items-move{
+    transition: all 0.5s ease;
+}
+
+</style>
---
+++ src/views/SingleElementView.vue
@@ -0,0 +1,32 @@
+<script setup>
+import {ref} from 'vue'
+const showGreed = ref(true)
+</script>
+
+<template>
+  <main class="p-10">
+    <div class="relative h-10">
+      <Transition name="fade" mode="out-in">
+        <h1 class="absolute" v-if="showGreed">Hello World <br> 2121</h1>
+        <h1 class="absolute" v-else="showGreed">Goodby World</h1>
+      </Transition>
+      <button class="mt-20 border rounded p-2" @click="showGreed = !showGreed">
+        toggle
+      </button>
+    </div>
+  </main>
+</template>
+<style scoped>
+.fade-enter-from,.fade-leave-to{
+  opacity: 0; 
+ 
+}
+.fade-enter-to,.fade-leave-from{
+  opacity: 1;
+}
+
+.fade-enter-active,.fade-leave-active{
+  transition: all 0.5s ease;
+}
+ 
+</style>
